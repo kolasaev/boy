7a24b3e8e7ba90c8161f204ce40bea6ea46c00a2
kolasaev <113944430+kolasaev@users.noreply.github.com>
2022-11-23 23:57:55 +0300

    Add files via upload

---
+++ script4.py
@@ -0,0 +1,70 @@
+up = [' ', 0, 1, 2]
+pole = [['-', '-', '-'],
+        ['-', '-', '-'],
+        ['-', '-', '-']]
+
+
+def game_pole(pole):
+    print(*(up))
+    for i in range(len(pole)):
+        print(str(i) + ' ' + ' '.join(pole[i]))
+
+
+def users_input(f, user):
+    while True:
+        koord = input(f"Ходит {user} .Введите координаты:").split()
+        x, y = map(int, koord)
+        if len(koord) != 2:
+            print('Введите два числа: ')
+            continue
+        if not (x >= 0 and x < 3 and y >= 0 and y < 3):
+            print('Вышли из диапозона: ')
+            continue
+        if f[x][y] != '-':
+            print('Клетка занята!')
+            continue
+        break
+    return x, y
+
+
+
+def win_position(f,user):
+    f_list=[]
+    print(f)
+    for l in f:
+        f_list+=l
+    print(f_list)
+    positions=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    indices = set([i for i, x in enumerate(f_list) if x == user])
+
+    for p in positions:
+        if len(indices.intersection(set(p)))==3:
+            return True
+    return False
+
+
+def start(field):
+
+    count=0
+    while True:
+        game_pole(field)
+        if count%2==0:
+            user = 'x'
+        else:
+            user = 'o'
+        if count<9:
+            x, y = users_input(field,user)
+            field[x][y] = user
+
+        elif count==9:
+            print ('Ничья')
+            break
+        if win_position(field,user):
+            print(f"Выйграл {user}")
+            break
+        count+=1
+
+
+field = [['-'] * 3 for _ in range(3)]
+
+start(field)
